880911fc9b9e9720cf7914829788e664c7763643
2223M1 <theonetrue2223@gmail.com>
2026-07-09 16:42:54 +0800

    Add Ninja Apathy card

---
+++ Cards/NinjaApathy.cs
@@ -0,0 +1,50 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using NinjaSlayer.Content;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace NinjaSlayer.Cards;
+
+[RegisterCard(typeof(NinjaSlayerCardPool))]
+public sealed class NinjaApathy : ModCardTemplate
+{
+    private const int energyCost = 1;
+    private const CardType type = CardType.Skill;
+    private const CardRarity rarity = CardRarity.Common;
+    private const TargetType targetType = TargetType.Self;
+    private const bool shouldShowInCardLibrary = true;
+
+    public override bool GainsBlock => true;
+
+    public override CardAssetProfile AssetProfile => new(
+        PortraitPath: "res://NinjaSlayer/images/cards/SmokeRead.png"
+    );
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new BlockVar(6, ValueProp.Move)
+    ];
+
+    protected override IEnumerable<IHoverTip> AdditionalHoverTips => [
+        HoverTipFactory.FromPower<ReboundPower>()
+    ];
+
+    public NinjaApathy() : base(energyCost, type, rarity, targetType, shouldShowInCardLibrary) { }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
+        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+        await PowerCmd.Apply<ReboundPower>(choiceContext, Owner.Creature, 1m, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Block.UpgradeValueBy(3);
+    }
+}
