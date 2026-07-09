3b65fd52421bc831e029dc173289a6c8b27d2b73
2223M1 <theonetrue2223@gmail.com>
2026-07-09 11:57:50 +0800

    Add I Hit attack card

---
+++ Cards/IHit.cs
@@ -0,0 +1,60 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Commands.Builders;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using NinjaSlayer.Content;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace NinjaSlayer.Cards;
+
+[RegisterCard(typeof(NinjaSlayerCardPool))]
+public sealed class IHit : ModCardTemplate
+{
+    private const int energyCost = 2;
+    private const CardType type = CardType.Attack;
+    private const CardRarity rarity = CardRarity.Common;
+    private const TargetType targetType = TargetType.AnyEnemy;
+    private const bool shouldShowInCardLibrary = true;
+
+    public override CardAssetProfile AssetProfile => new(
+        PortraitPath: "res://NinjaSlayer/images/cards/StrikeNinjaSlayer.png"
+    );
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DamageVar(12, ValueProp.Move),
+        new DynamicVar("Energy", 2)
+    ];
+
+    protected override IEnumerable<IHoverTip> AdditionalHoverTips => [
+        HoverTipFactory.FromCard<ChadoCard>()
+    ];
+
+    public IHit() : base(energyCost, type, rarity, targetType, shouldShowInCardLibrary) { }
+
+    protected override bool ShouldGlowGoldInternal => NinjaSlayerActions.ChadoExhaustedThisTurn(this);
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target);
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .FromCard(this, cardPlay)
+            .WithDefectStrikeHitFx()
+            .WithAttackerAnim("Attack", Owner.Character.AttackAnimDelay)
+            .Targeting(cardPlay.Target)
+            .Execute(choiceContext);
+
+        if (NinjaSlayerActions.ChadoExhaustedThisTurn(this))
+        {
+            await PlayerCmd.GainEnergy(DynamicVars["Energy"].BaseValue, Owner);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(4);
+    }
+}
