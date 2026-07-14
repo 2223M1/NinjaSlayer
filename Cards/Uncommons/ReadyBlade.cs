fea3627f699bf711bc3dabf55d44d264b7420e75
2223M1 <theonetrue2223@gmail.com>
2026-07-14 10:05:17 +0800

    Rework Ready Blade around prepared shuriken

--- Cards/Uncommons/ReadyBlade.cs
+++ Cards/Uncommons/ReadyBlade.cs
@@ -3,7 +3,7 @@ using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-using NinjaSlayer.Code.Commands;
+using NinjaSlayer.Afflictions;
 using NinjaSlayer.Content;
 using STS2RitsuLib.Interop.AutoRegistration;
 using STS2RitsuLib.Scaffolding.Content;
@@ -19,12 +19,12 @@ public sealed class ReadyBlade : NinjaSlayerCardTemplate
     private const bool shouldShowInCardLibrary = true;
 
     protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new CardsVar(3),
+        new CardsVar(1),
         new ShurikenVar(3)
     ];
 
     protected override IEnumerable<IHoverTip> AdditionalHoverTips => [
-        HoverTipFactory.FromKeyword(NinjaSlayerKeywords.Scry)
+        .. HoverTipFactory.FromAffliction<PreparedAffliction>()
     ];
 
     public ReadyBlade() : base(energyCost, type, rarity, targetType, shouldShowInCardLibrary) { }
@@ -32,13 +32,18 @@ public sealed class ReadyBlade : NinjaSlayerCardTemplate
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
         await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
-        await ScryCmd.Execute(choiceContext, Owner, DynamicVars.Cards.IntValue);
-        await NinjaSlayerActions.AddGeneratedShuriken(choiceContext, Owner, DynamicVars.Shuriken().IntValue, PileType.Draw, position: CardPilePosition.Top);
+        await NinjaSlayerActions.AddGeneratedShuriken(
+            choiceContext,
+            Owner,
+            DynamicVars.Shuriken().IntValue,
+            PileType.Draw,
+            position: CardPilePosition.Top,
+            prepare: true);
+        await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.BaseValue, Owner);
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars.Cards.UpgradeValueBy(1);
         DynamicVars.Shuriken().UpgradeValueBy(1);
     }
 }
